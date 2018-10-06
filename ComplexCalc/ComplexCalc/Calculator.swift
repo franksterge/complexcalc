cc1fb8f25c19efc33fcedf643e30ab3cb99fa2b7
franksterge <fangwg@uw.edu>
2018-10-06 01:38:36 -0700

    added comment

--- ComplexCalc/ComplexCalc/Calculator.swift
+++ ComplexCalc/ComplexCalc/Calculator.swift
@@ -9,20 +9,18 @@
 //Info 449
 //10.8.18
 //this program is a complex calculator that is capable to do various operations
-//for both possitive and negative integers including addtion, subtraction, multiplication, and
-//division as well as complex operation including counting the number of
-//integers in a given list, taking the average for the given list, and taking factorial for a given
-//integer.
-
+//for possitive and negative integers, list of integers, and cartisan points.
 
 import Foundation
 
 // All your work will go in here
 class Calculator {
+    //adds two integers together and returns the value
     public func add (lhs: Int, rhs: Int) -> Int {
         return Int(lhs + rhs)
     }
     
+    //gets the sum of the list of the integer
     public func add(_ list: [Int]) -> Int {
         var result = 0
         for i in list {
@@ -31,31 +29,39 @@ class Calculator {
         return result
     }
     
+    //adds two cartisan points together and returns the result
     public func add (lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int) {
         return (lhs.0 + rhs.0, lhs.1 + rhs.1)
     }
     
+    //adds two cartisan points in the form of dictionaries together and returns the result
     public func add (lhs: [String: Int], rhs: [String: Int]) -> [String: Int] {
         return ["x": lhs["x"]! + rhs["x"]!,
                 "y": lhs["y"]! + rhs["y"]!] as [String: Int]
     }
+    
+    //subtracts two integers and returns the result
     public func subtract (lhs: Int, rhs: Int) -> Int {
         return Int(lhs - rhs)
     }
     
+    //subtracts two cartisan points and returns the result
     public func subtract (lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int) {
         return (lhs.0 - rhs.0, lhs.1 - rhs.1)
     }
     
+    //subtracts two cartisan points in the form of dictionaries together and returns the result
     public func subtract (lhs: [String: Int], rhs: [String: Int]) -> [String: Int] {
         return ["x": lhs["x"]! - rhs["x"]!,
                 "y": lhs["y"]! - rhs["y"]!] as [String: Int]
     }
     
+    //multiplies two integers and returns the result
     public func multiply (lhs: Int, rhs: Int) -> Int {
         return Int(lhs * rhs)
     }
     
+    //cumulatively multiplies a list of integer and returns the result
     public func multiply (_ list: [Int]) -> Int {
         var result: Int
         switch list.count {
@@ -64,12 +70,13 @@ class Calculator {
         default:
             result = list[0]
             for i in 1...list.count - 1 {
-                result = multiply(lhs: result, rhs: list[i])
+                result *= list[i]
             }
         }
         return result
     }
     
+    //divides the first integer passed in by the second and returns the result.
     public func divide (lhs: Int, rhs: Int) -> Int {
         switch rhs {
         case 0:
@@ -79,22 +86,27 @@ class Calculator {
         }
     }
     
+    //counts the size of the list of integer passed in and returns the result.
     public func count (_ list: [Int]) -> Int {
         return list.count
     }
     
+    //calculates the arithmatic mean of the list of integer passed in and returns the result.
     public func avg (_ list: [Int]) -> Int {
         return add(list) / Int(list.count)
     }
     
+    //takes in two integers and applies the given custom operation and returns the result.
     public func mathOp(lhs: Int, rhs: Int, op: (Int, Int) -> Int) -> Int {
         return op(lhs, rhs)
     }
     
+    //takes in a base integer and a list of the integer and applies the given custom operation and returns the result.
     public func mathOp(args: [Int], beg: Int, op: (Int, Int) -> Int) -> Int {
         return opHelper(args, beg, op, 0)
     }
     
+    //helper method that calculate a custom operation with given list of a integer, the current index of that list, and carries out the result and returns it.
     private func opHelper(_ args:[Int], _ result: Int, _ op: (Int, Int) -> Int, _ index: Int) -> Int {
         switch index {
         case args.count:
